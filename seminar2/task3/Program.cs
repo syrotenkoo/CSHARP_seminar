b3e504c42a4c14e738a475fcd2d0ef7ed6b04f49
olya <fgfg>
2024-02-25 21:15:02 +0300

    Простые алгоритмы. Задача 3

---
+++ seminar2/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход целое число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 10 && number < 99) {
+    int leftDigit = number / 10;
+    int rightDigit = number % 10;
+
+    if(leftDigit > rightDigit) {
+        System.Console.WriteLine(leftDigit);
+    } else if(leftDigit < rightDigit) {
+        System.Console.WriteLine(rightDigit);
+    } else {
+        System.Console.WriteLine("Цифры равны");
+    }
+} else {
+    System.Console.WriteLine("Введено некорректное число!");
+}

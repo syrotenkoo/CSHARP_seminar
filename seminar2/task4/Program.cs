87eb709ef9c56a8f78250edc175ad9ef4aa1097d
olya <fgfg>
2024-02-25 21:31:30 +0300

    Простые алгоритмы. Задача 4

---
+++ seminar2/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number / 10 > 0) {
+    System.Console.Write($"{number % 10}, ");
+    number /= 10;
+}
+
+System.Console.Write(number);
